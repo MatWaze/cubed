755a88b524ac9421aece800733e54907f8aca2d0
MatWaze <mamazari@c2r12s4.42yerevan.am>
2024-08-09 11:33:43 +0400

    Little error checking.

--- cub3d.c
+++ cub3d.c
@@ -3,28 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: matevos <matevos@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mamazari <mamazari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/10 20:30:01 by matevos           #+#    #+#             */
-/*   Updated: 2024/07/10 20:31:25 by matevos          ###   ########.fr       */
+/*   Created: 2024/07/10 20:30:01 by mamazari          #+#    #+#             */
+/*   Updated: 2024/08/09 11:29:03 by mamazari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <fcntl.h>
+
+#include "parsing/parsing.h"
+#include "common/common.h"
+#include "libft/libft.h"
 
 int	main(int argc, char **argv)
 {
-	int	ans;
-
-	(void)argc;
-	(void)argv;
+	int		ans;
+	t_cub	cubed;
 
 	ans = 0;
 	if (argc == 2)
 	{
-		printf("");
+		cubed.fd = open(argv[1], O_RDONLY);
+		if (check_file(&cubed.fd) == 0)
+		{
+			ans = get_textures_colors(&cubed);
+			if (ans == 1)
+				printf("Error with texture(s) or color(s).\n");
+			else
+			{
+				// map check
+			}
+		}
 	}
 	else
-		ans = 1;
+		printf("Usage: ./cub3d path_to_cub_file.\n");
 	return (ans);
 }
