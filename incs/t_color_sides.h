ed7594c9365b4c721deaad8594738b2193d44c29
MatWaze <mamazari@c2r12s4.42yerevan.am>
2024-08-10 11:38:54 +0400

    Changed t_color_sides.

--- incs/t_color_sides.h
+++ incs/t_color_sides.h
@@ -6,7 +6,7 @@
 /*   By: mamazari <mamazari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 12:07:11 by mamazari          #+#    #+#             */
-/*   Updated: 2024/08/09 13:13:41 by mamazari         ###   ########.fr       */
+/*   Updated: 2024/08/10 11:37:37 by mamazari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@
 
 typedef struct s_color_sides
 {
-	int		north_found;
-	int		south_found;
-	int		west_found;
-	int		east_found;
-	int		floor_found;
-	int		ceiling_found;
+	int		north_found : 1;
+	int		south_found : 1;
+	int		west_found : 1;
+	int		east_found : 1;
+	int		floor_found : 1;
+	int		ceiling_found : 1;
 	char	*north;
 	char	*south;
 	char	*west;
