94612379f05fef98a8d94107579878946ab8378f
MatWaze <coolmartun@gmail.com>
2024-08-13 18:27:47 +0400

    Added header.

--- cub3d.c
+++ cub3d.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "parsing/parsing.h"
 #include "common/common.h"
