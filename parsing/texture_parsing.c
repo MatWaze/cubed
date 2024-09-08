19e38671aa68a054c2f6886e954e7621db9e4420
MatWaze <coolmartun@gmail.com>
2024-09-08 21:39:44 +0400

    get_textures_colors returns 10 on malloc failure.

--- parsing/texture_parsing.c
+++ parsing/texture_parsing.c
@@ -6,7 +6,7 @@
 /*   By: matevos <matevos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 14:16:52 by mamazari          #+#    #+#             */
-/*   Updated: 2024/09/05 20:07:39 by matevos          ###   ########.fr       */
+/*   Updated: 2024/09/08 19:42:46 by matevos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,49 @@
 #include "t_cub.h"
 
 int		is_valid_str(char *line, char **s, t_cub *args);
-int		free_mem(char *line, char *trim, char **split);
+int		fr(char *line, char *trim, char **split);
 
-int	free_mem(char *line, char *trim, char **split)
+void	any_null(t_cub *args, int *res, int *ans)
 {
-	if (line != NULL)
+	if (args->col_sides.east_found == 0 && \
+	args->col_sides.east == NULL)
+	{
+		*ans = 10;
+		*res = 10;
+	}
+	else if (args->col_sides.west_found == 0 && \
+	args->col_sides.west == NULL)
+	{
+		*ans = 10;
+		*res = 10;
+	}
+	else if (args->col_sides.south_found == 0 && \
+	args->col_sides.south == NULL)
+	{
+		*ans = 10;
+		*res = 10;
+	}
+	else if (args->col_sides.north_found == 0 && \
+	args->col_sides.north == NULL)
+	{
+		*ans = 10;
+		*res = 10;
+	}
+}
+
+int	fr(char *line, char *trim, char **split)
+{
+	if (line)
 	{
 		free(line);
-		line = NULL;
 	}
-	if (trim != NULL)
+	if (trim)
 	{
 		free(trim);
-		trim = NULL;
 	}
-	if (split != NULL)
+	if (split)
 	{
 		free_arr(split);
-		split = NULL;
 	}
 	return (0);
 }
@@ -44,27 +69,28 @@ int	free_mem(char *line, char *trim, char **split)
 int	get_textures_colors(t_cub *args, int *line_count)
 {
 	int		count;
-	char	*line;
-	char	*trim;
-	char	**split;
+	char	*l;
+	char	*t;
+	char	**s;
 
 	count = 0;
 	while (count != 6)
 	{
 		(*line_count)++;
-		line = get_next_line(args->fd);
-		if (line == NULL)
+		l = get_next_line(args->fd);
+		if (l == NULL)
 			break ;
-		trim = ft_strtrim(line, "\t\b ");
-		split = ft_ssplit(line, " \t\n");
-		if (split_count(split) == 0 && free_mem(line, trim, split) == 0)
+		t = ft_strtrim(l, "\t\b ");
+		s = ft_ssplit(l, " \t\n");
+		if ((t == NULL || s == NULL) && fr(l, t, s) == 0)
+			return (10);
+		if (split_count(s) == 0 && fr(l, t, s) == 0)
 			continue ;
-		else if (is_valid_str(line, split, args) == 1)
+		else if (is_valid_str(l, s, args) == 1)
 			count++;
-		else if ((ft_strcmp(trim, "\n") != 0 && count != 6) \
-			&& free_mem(line, trim, split) == 0)
+		else if ((ft_strcmp(t, "\n") != 0 && count != 6) && fr(l, t, s) == 0)
 			return (1);
-		free_mem(line, trim, split);
+		fr(l, t, s);
 	}
 	return (count != 6);
 }
