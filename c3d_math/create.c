2eb0e053093dcfa605d64efff12ebf7843abe58b
MatWaze <coolmartun@gmail.com>
2024-09-16 16:04:03 +0400

    Norminetted.

--- c3d_math/create.c
+++ c3d_math/create.c
@@ -17,13 +17,13 @@
 #include "error/error.h"
 #include "error/codes.h"
 #include <stdio.h>
+
 bool	create_mat(t_mat *mat, unsigned int w, unsigned int h, t_err *error)
 {
 	char	*row;
 
 	if (track(error, "create_mat") && check_err(error, w && h, C3D_INV_PAR))
 	{
-		printf("%d, %d\n", w, h);
 		mat->m = (char **)malloc(sizeof(char) * w * h + sizeof(char *) * h);
 		if (check_err(error, mat->m != NULL, C3D_ALL))
 		{
@@ -38,5 +38,6 @@ bool	create_mat(t_mat *mat, unsigned int w, unsigned int h, t_err *error)
 		}
 		untrack(error);
 	}
+	print_trace(error);
 	return (!error->error);
 }
