317f53c316ba7a96b9cdacd6d43708d7a15773ca
MatWaze <coolmartun@gmail.com>
2024-10-04 16:29:38 +0400

    Made matrix not pointer

--- parsing/t_cub.h
+++ parsing/t_cub.h
@@ -6,13 +6,14 @@
 /*   By: mamazari <mamazari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 12:07:30 by mamazari          #+#    #+#             */
-/*   Updated: 2024/08/10 18:57:59 by mamazari         ###   ########.fr       */
+/*   Updated: 2024/10/04 16:23:36 by mamazari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef T_CUB_H
 # define T_CUB_H
 
+# include "c3d_math/t_mat.h"
 # include "common/t_color_sides.h"
 
 typedef struct s_cub
@@ -21,5 +22,6 @@ typedef struct s_cub
 	int				fd;
 	char			*name;
 	char			**map;
+	t_mat			mat;
 }			t_cub;
 #endif
