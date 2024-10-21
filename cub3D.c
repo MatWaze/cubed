6928e090e69aab21f9b3b5c929417a64018db4f0
zanikin <zaharanikinmai@gmail.com>
2024-10-21 16:20:49 +0400

    norm

--- cub3D.c
+++ cub3D.c
@@ -6,7 +6,7 @@
 /*   By: zanikin <zanikin@student.42yerevan.am>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 20:30:01 by mamazari          #+#    #+#             */
-/*   Updated: 2024/10/21 15:54:41 by zanikin          ###   ########.fr       */
+/*   Updated: 2024/10/21 16:20:05 by zanikin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,35 @@
 #include <stdio.h>
 #include <unistd.h>
 
-bool		set_level(t_game *game, t_cub *cub);
+static bool	set_level(t_game *game, t_cub *cub);
 void		free_game(t_render *r, t_mat *map, t_mat *states);
 
 static bool	read_textures(t_game *game, t_cub *cub);
+static void	start_game(t_game *game, t_cub *cub, int tmp);
 
-bool	set_level(t_game *game, t_cub *cub)
+int	main(int argc, char **argv)
+{
+	static t_cub	cub = {0};
+	static t_game	game = {.e = {0}, .r = {0}, .states = {0}, .door_hit = {0}};
+
+	game.r.mlx = mlx_init();
+	if (track(&game.e, "main") && check_err(&game.e, argc == 2, \
+	C3D_MAIN_INV_PARAM) && check_err(&game.e, access(argv[1], R_OK) == 0, \
+	PARSING_FILE_NOT_OPEN) && check_err(&game.e, is_cub(argv[1]) == 1, \
+	MAIN_INV_FILE_NAME))
+	{
+		if (validation(argv[1], &cub, &game.e) && set_imgs(&game) && set_level(\
+		&game, &cub) && create_mat(&game.states, game.map->w, game.map->h, \
+		&game.e) && check_err(&game.e, game.r.mlx != NULL, MLX_INIT) && \
+		check_err(&game.e, argc == 2, C3D_MAIN_INV_PARAM))
+			start_game(&game, &cub, 0);
+	}
+	untrack(&game.e);
+	print_trace(&game.e);
+	return (game.e.error);
+}
+
+static bool	set_level(t_game *game, t_cub *cub)
 {
 	if (track(&game->e, "test_level"))
 	{
@@ -112,52 +135,31 @@ void	free_game(t_render *r, t_mat *map, t_mat *states)
 	free_mat(states);
 }
 
-int	main(int argc, char **argv)
+static void	start_game(t_game *game, t_cub *cub, int tmp)
 {
-	static t_cub	cub = {0};
-	static t_game	game = {.e = {0}, .r = {0}, .states = {0}, .door_hit = {0}};
-	int				tmp;
-	int				valid;
-
-	valid = 0;
-	(void)argv;
-	game.r.mlx = mlx_init();
-	if (track(&game.e, "main") && check_err(&game.e, argc == 2, \
-	C3D_MAIN_INV_PARAM) && check_err(&game.e, access(argv[1], R_OK) == 0, \
-	PARSING_FILE_NOT_OPEN) && check_err(&game.e, is_cub(argv[1]) == 1, \
-	MAIN_INV_FILE_NAME))
+	set_mat(&game->states, 0);
+	game->r.win = mlx_new_window(game->r.mlx, WIN_WIDTH, WIN_HEIGHT,
+			"cub3D");
+	game->r.img = mlx_new_image(game->r.mlx, WIN_WIDTH, WIN_HEIGHT);
+	if (check_err(&game->e, game->r.win != NULL, MLX_ALLOC)
+		&& check_err(&game->e, game->r.img != NULL, MLX_ALLOC))
 	{
-		if (validation(argv[1], &cub, &game.e) && set_imgs(&game) && set_level(&game, &cub) && create_mat(&game.states, game.map->w, game.map->h, &game.e) && check_err(&game.e, game.r.mlx != \
-		NULL, MLX_INIT) && check_err(&game.e, argc == 2, C3D_MAIN_INV_PARAM))
-		{
-			set_mat(&game.states, 0);
-			game.r.win = mlx_new_window(game.r.mlx, WIN_WIDTH, WIN_HEIGHT,
-					"cub3D");
-			game.r.img = mlx_new_image(game.r.mlx, WIN_WIDTH, WIN_HEIGHT);
-			if (check_err(&game.e, game.r.win != NULL, MLX_ALLOC)
-				&& check_err(&game.e, game.r.img != NULL, MLX_ALLOC))
-			{
-				game.r.img_buff = (int *)mlx_get_data_addr(game.r.img, &tmp,
-						&tmp, &tmp);
-				if (cub.map)
-					free_arr(cub.map);
-				if (free_return(cub.col_sides.north) && free_return(\
-				cub.col_sides.south) && free_return(cub.col_sides.east) \
-				&& free_return(cub.col_sides.west))
-					;
-				mlx_do_key_autorepeaton(game.r.mlx);
-				mlx_hook(game.r.win, KeyPress, 0, key_hook, &game);
-				mlx_hook(game.r.win, DestroyNotify, 0, exit_game, &game);
-				mlx_loop_hook(game.r.mlx, loop_hook, &game);
-				mlx_mouse_move(game.r.win, WIN_WIDTH / 2, WIN_HEIGHT / 2);
-				mlx_mouse_hide();
-				render(&game);
-				draw_minimap(&game);
-				mlx_loop(game.r.mlx);
-			}
-		}
+		game->r.img_buff = (int *)mlx_get_data_addr(game->r.img, &tmp,
+				&tmp, &tmp);
+		if (cub->map)
+			free_arr(cub->map);
+		if (free_return(cub->col_sides.north) && free_return(\
+		cub->col_sides.south) && free_return(cub->col_sides.east) \
+		&& free_return(cub->col_sides.west))
+			;
+		mlx_do_key_autorepeaton(game->r.mlx);
+		mlx_hook(game->r.win, KeyPress, 0, key_hook, game);
+		mlx_hook(game->r.win, DestroyNotify, 0, exit_game, game);
+		mlx_loop_hook(game->r.mlx, loop_hook, game);
+		mlx_mouse_move(game->r.win, WIN_WIDTH / 2, WIN_HEIGHT / 2);
+		mlx_mouse_hide();
+		render(game);
+		draw_minimap(game);
+		mlx_loop(game->r.mlx);
 	}
-	untrack(&game.e);
-	print_trace(&game.e);
-	return (game.e.error);
 }
